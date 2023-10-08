699b62669ea6060ebc15b1f976312a1621eb6eee
Jihadned <jjadned@gmail.com>
2023-10-08 13:20:45 -0400

    Basic Nav"

---
+++ formatting/script.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    count = 0;
+    const navbutton = document.getElementById("show");
+    navbutton.onclick = shownav;
+  }
+
+shownav = () => {
+    x = document.getElementById("navbar");
+    if (count == 0) {
+        x.classList.remove("display");
+        count = 1;
+    } else {
+        x.classList.add("display");
+        count = 0;
+    }
+    console.log("Hellos!");
+}
