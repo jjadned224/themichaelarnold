cd8e8a94d46c386768b027c59d4fce6d7ead5bb8
Jihadned <jjadned@gmail.com>
2023-10-14 13:52:58 -0400

    Home done

--- formatting/script.js
+++ formatting/script.js
@@ -2,7 +2,42 @@ window.onload = () => {
     count = 0;
     const navbutton = document.getElementById("show");
     navbutton.onclick = shownav;
-  }
+
+    document.getElementById("currentQ").innerHTML = quotes[0];
+    document.getElementById("names").innerHTML = names[0];
+    document.getElementById("titles").innerHTML = title[0];
+    rotator(true);
+
+    const but1 = document.getElementById("button1");
+    const but2 = document.getElementById("button2");
+    but1.onclick = react;
+    but2.onclick = react;
+    
+    setInterval(() => {
+        rotator(false);
+}, 10000);
+
+}
+
+react = () => {
+    rotator(false)
+}
+
+currentbutton = (current = Boolean) => {
+    console.log(current);
+    if (current == true) {
+        document.getElementById("button1").classList.remove("pressed");
+        document.getElementById("button1").classList.add("buttonunpress");
+        document.getElementById("button2").classList.add("pressed");
+        document.getElementById("button2").classList.remove("buttonunpress");
+    } else {
+        document.getElementById("button1").classList.add("pressed");
+        document.getElementById("button1").classList.remove("buttonunpress");
+        document.getElementById("button2").classList.remove("pressed");
+        document.getElementById("button2").classList.add("buttonunpress");
+    }
+}
+  
 
 shownav = () => {
     x = document.getElementById("navbar");
@@ -13,4 +48,32 @@ shownav = () => {
         x.classList.add("display");
         count = 0;
     }
+}
+
+const quotes = ['“This is the best presentation that I have heard in this club and one of the best workshops I have participated in the District. I actually participated and I am a notorious non-participant, your energy engaged us.”','“Michael mentored me through Six Sigma training and was instrumental in helping develop my skills in problem solving. Michael was able to help myself and another colleague examine the data using various problem-solving methods to look for all possible solutions, reducing potential injuries within the facility.”'];
+
+const names = ['Allison B.', 'Angela S.'];
+
+const title = ['Toastmaster of 25 years','EHS Coordinator'];
+
+rotator = (test = Boolean) => {
+    if (test == true) {
+        i = 0;
+} else {
+    if(i > 1){
+        i = 0;
+        currentbutton(false);
+    } else {
+        currentbutton(true);
+    }
+}
+    const out = document.querySelector("#currentQ");
+    const outname = document.querySelector("#names");
+    const outtitle = document.querySelector("#titles");
+    
+    out.innerHTML = quotes[i];
+    outname.innerHTML = names[i];
+    outtitle.innerHTML = title[i];
+    i++
+
 }
