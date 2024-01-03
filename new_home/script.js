45b550cdb4cfef99aaa55da87d65accd3e6f5a86
Jihadned <jjadned@gmail.com>
2024-01-03 02:12:17 -0500

    temp new_home2

--- new_home/script.js
+++ new_home/script.js
@@ -3,6 +3,8 @@ window.onload = () => {
     const navbutton = document.getElementById("show");
     navbutton.onclick = shownav;
 
+  showBooks();
+
     document.getElementById("currentQ").innerHTML = quotes[0];
     document.getElementById("names").innerHTML = names[0];
     document.getElementById("titles").innerHTML = title[0];
@@ -116,3 +118,70 @@ const getEmailResult = async (e) => {
   }
 };
 
+
+
+shownav = () => {
+  x = document.getElementById("navbar");
+  if (count == 0) {
+      x.classList.remove("display");
+      count = 1;
+  } else {
+      x.classList.add("display");
+      count = 0;
+  }
+}
+
+const getBooks = async () => {
+  const url = "https://jjadned224.github.io/themichaelarnold/new_home/books.json";
+
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const showBooks = async () => {
+  let books = await getBooks();
+  let BookList = document.getElementById("book-lists");
+
+  books.forEach((book) => {
+    BookList.append(getBookItem(book));
+  });
+};
+
+const getBookItem = (book) => {
+  let title = document.createElement("div");
+  let h1 = document.createElement("h1");
+  h1.innerText = book.name;
+  title.append(h1);
+
+  let section = document.createElement("section");
+  section.classList.add("rower");
+  
+  let row = document.createElement("div");
+
+  let img = document.createElement("img");
+  img.src = book.img;
+  section.append(img);
+
+  let ul = document.createElement("ul");
+  
+  section.append(ul);
+  ul.append(getLi(`Author: ${book.Author}`));
+  ul.append(getLi(`Published: ${book.Published}`));
+  ul.append(getLi(`Genres: ${book.genres}`));
+  ul.append(getLi(`Page Count: ${book.page}`));
+  ul.append(getLi(`Description: ${book.description}`));
+  
+  row.appendChild(title);
+  row.appendChild(section);
+  return row;
+};
+
+const getLi = (data) => {
+  const li = document.createElement("li");
+  li.textContent = data;
+  return li;
+};
